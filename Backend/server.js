8c2954c97ac3e5a6f4d444e3686456825db9255f
Kelvin-Mutinda <mwendwamutinda24@gmail.com>
2026-02-27 17:36:17 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,28 @@
-
-const express=require('express');
-const mongoose=require('mongoose');
-const cors=require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 const authRoutes = require('./routes/auth'); 
 
-const app=express();
+const app = express();
 
-app.use(cors({ origin: ['https://predictionsite-gr8r.vercel.app/'] , 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true }));
+
+app.use(cors({
+  origin: "https://predictionsite-gr8r.vercel.app", // no trailing slash
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use(express.json());
+
 
 app.use('/auth', authRoutes);
 
 
-
-
 mongoose.connect(process.env.MONGO_URI)
-  .then(()=>console.log("MongoDb Connected successfully"))
-  .catch(err=>console.log(err))
+  .then(() => console.log("MongoDb Connected successfully"))
+  .catch(err => console.log(err));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
